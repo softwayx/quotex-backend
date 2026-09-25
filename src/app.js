dfe3a3f11a79bb89f9505808353a01f71290e860
Akash <proftakas@gmail.com>
2026-09-26 00:33:35 +0530

    first commit-2

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ export const createApp = () => {
   app.use(cookieParser());
   app.use(pinoHttp({ logger, autoLogging: { ignore: (req) => req.url === '/api/v1/public/health' } }));
 app.get("/", (req, res) => {
-  res.send("server is running on port 55001");
+  res.send("server is running on port 5500 riskquo");
 });
   app.use('/api/v1', routes);
 
